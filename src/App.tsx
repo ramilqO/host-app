3b19e6f86d541f076b706f0521f61b9bcf242dae
Ramil Osmanov <osmanovramil823@gmail.com>
2024-03-14 18:18:13 +0600

    host-app commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Micro1 from "@rossmanov/micro-1/src/App.tsx";
+import Micro2 from "@rossmanov/micro-2/src/App.tsx";
+
+function App() {
+  return (
+    <div>
+      <h1>I AM HOST APP!!!</h1>
+      <Micro1 />
+      <Micro2 />
+    </div>
+  );
+}
+
+export default App;
